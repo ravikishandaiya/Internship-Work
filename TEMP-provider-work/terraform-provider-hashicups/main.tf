e0c80574237084f95e8aee9487189de7435941bd
Ravi Kishan <ravikishandaiya@gmail.com>
2021-04-14 11:59:10 +0530

    Update main.tf

--- TEMP-provider-work/terraform-provider-hashicups/main.tf
+++ TEMP-provider-work/terraform-provider-hashicups/main.tf
@@ -6,3 +6,27 @@ terraform {
     }
   }
 }
+
+provider "hashicups" {
+  username = "education"
+  password = "test123"
+}
+
+resource "hashicups_order" "edu" {
+  items {
+    coffee {
+      id = 3
+    }
+    quantity = 2
+  }
+  items {
+    coffee {
+      id = 2
+    }
+    quantity = 2
+  }
+}
+
+output "edu_order" {
+  value = hashicups_order.edu
+}
