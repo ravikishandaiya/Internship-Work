d1656430e10bc4cd86c314a7124674ca4b147f19
Ravi Kishan <ravikishandaiya@gmail.com>
2021-04-21 11:42:43 +0530

    Update client.go

--- terraform-provider-zoom/client/client.go
+++ terraform-provider-zoom/client/client.go
@@ -1,22 +1,25 @@
+
+
+
 package client
 
 import (
-	"bytes"
+	//"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
 )
 
-type Client struct {
+type client struct {
 	address string
 	token string
 }
 
 func NewClient(address string, token string) *client {
-	return &Client{
+	return &client{
 		address: address,
-		token: token
+		token: token,
 	}
 }
 
@@ -78,7 +81,7 @@ func handleReadRequest(url string, httpMethod string, JWT_Token string) (respons
 // get list for all user
 
 func ListUser(url, token string) {
-	res, err := handleReadRequest(url, http.MethodGet, tokken)
+	res, err := handleReadRequest(url, http.MethodGet, token)
 	if err != nil {
 		fmt.Println("Error\n")
 	}
@@ -95,5 +98,86 @@ func ListUser(url, token string) {
 	}
 }
 
+// struct to send data to html body and for mapping data from responce 
+
+type Userinfo struct {
+	Email     string `json:"email"`
+	Type      int    `json:"type"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+type CreateUserRequest struct {
+	Action   string   `json:"action"`
+	UserInfo Userinfo `json:"user_info"`
+}
+
+type CreateUserResponse struct {
+	Id        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Type      int    `json:"type"`
+}
+
+//Handle create request
+
+func handleCreateRequest(url string, JWT_Token string, httpMethod string, body []byte) (response []byte, err error) {
+	httpClient := &http.Client{}
+	
+	var req *http.Request
+	req, err = http.NewRequest(httpMethod, url, bytes.NewBuffer(body))
+	if err != nil {
+		return 
+	}
+
+	req.Header.Add("Authorization", "Bearer "+JWT_Token)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	response, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	return
+
+}
+
+
+
+// function to add new user
+
+
+func CreateUser(url string, token string, UserRequestData CreateUserRequest) {
+
+	var reqBody []byte
+	var err error
+	reqBody, err = json.Marshal(UserRequestData)
+	if err != nil {
+		return
+	}
+
+	httpMethod := http.MethodPost
+
+	var b []byte
+	b, err = handleCreateRequest(url, token, httpMethod, reqBody)
+
+	if err != nil {
+		return
+	}
+
+	var createUserResponse CreateUserResponse
+	err = json.Unmarshal(b, &createUserResponse)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(createUserResponse)
+}
 
 
