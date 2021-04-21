9b8f59053491b6a7f34a1b5654b09edb557185c7
Ravi Kishan <ravikishandaiya@gmail.com>
2021-04-21 11:44:10 +0530

    Add files via upload

---
+++ terraform-provider-zoom/provider/provider.go
@@ -0,0 +1,32 @@
+package provider
+
+import (
+	"github.com.hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"zoom/client"
+)
+
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"address": {
+				Type: schema.TypeString,
+				Required: true,
+			},
+			"token": {
+				Type: schema.TypeString,
+				Required: true,
+			},
+		},
+		ResourceMap: map[string]*schema.Resource{
+			"provider_user": resourceUser(),
+		},
+		ConfigureFunc: providerConfigure,
+	}
+} 
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	address := d.Get("address").(string)
+	token := d.Get("token").(string)
+	return client.NewClient(address, token), nil
+}
