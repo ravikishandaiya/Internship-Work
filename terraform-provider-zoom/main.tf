9faaeeb73c69b00a0031db269eed95c666e8e354
Ravi Kishan <ravikishandaiya@gmail.com>
2021-04-23 17:25:16 +0530

    Update main.tf

--- terraform-provider-zoom/main.tf
+++ terraform-provider-zoom/main.tf
@@ -9,8 +9,9 @@ terraform {
 
 provider "zoom" {
     //address = "https://api.zoom.us/v2/users/"
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MTk3NzQ4NjAsImlhdCI6MTYxOTE3MDA1Nn0.EgucHFpdFPjoHKMc4dSepij0NohuMGBnFUSxdcbZc3w"
-}
+    token = "token"
+  
+  }
 
 resource "zoom_user" "user2" {
     //firstname = "Ravi"
